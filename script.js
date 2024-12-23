0255d899e722f3d5213187f064def8521cecf246
Agustin <agustin.lovagnini.v@gmail.com>
2024-12-23 19:13:51 -0300

    agregando boton flotante

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const buttonHamburguesa = document.querySelector('.menu-hamburguesa');
 const menu = document.querySelector('.redes-sociales');
+const programacionDiaria = document.querySelector('.programacion-diaria-container');
+const shopLink = document.querySelector('.shop');
+const redesSociales = document.querySelector('.redes-sociales');
+
 
 buttonHamburguesa.addEventListener('click', () => {
     menu.classList.toggle('active');
     buttonHamburguesa.classList.toggle('active');
-
 
     const icon = buttonHamburguesa.querySelector('i');
     if (menu.classList.contains('active')) {
@@ -14,5 +17,30 @@ buttonHamburguesa.addEventListener('click', () => {
         icon.classList.remove('fa-times');
         icon.classList.add('fa-bars');
     }
-
 });
+
+
+function ajustarMenu() {
+    if (window.innerWidth <= 600) {
+        if (!redesSociales.contains(programacionDiaria)) {
+            redesSociales.appendChild(programacionDiaria); 
+        }
+        if (!redesSociales.contains(shopLink)) {
+            redesSociales.appendChild(shopLink);
+        }
+    } else {
+        const navPrincipal = document.querySelector('.nav-principal .nav-container');
+        const navSecundario = document.querySelector('.nav-secundario');
+
+        if (!navPrincipal.contains(programacionDiaria)) {
+            navPrincipal.appendChild(programacionDiaria);
+        }
+
+        if (!navSecundario.contains(shopLink)) {
+            navSecundario.appendChild(shopLink);
+        }
+    }
+}
+
+ajustarMenu();
+window.addEventListener('resize', ajustarMenu);
