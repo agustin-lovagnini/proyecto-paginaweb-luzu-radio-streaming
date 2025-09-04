a91161af67ad75f9e533f9d97f6647a241d5f123
Agustin Alberto Varela Lovagnini <agustin.lovagnini.v@gmail.com>
2025-09-03 21:39:28 -0300

    agregue un contenido que saque de js

--- script.js
+++ script.js
@@ -18,6 +18,29 @@ buttonHamburguesa.addEventListener('click', () => {
     }
 });
 
+const programas = document.querySelectorAll('.programacion-diaria-programa');
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+programas[0].classList.add('programa-visible');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !entry.target.classList.contains('programa-visible')) {
+            entry.target.classList.add('programa-visible');
+        }
+    });
+}, { threshold: 0.1 });
+
+programas.forEach(programaProgramacionDiaria => observer.observe(programaProgramacionDiaria));
+
+function getVisibleProgramas() {
+    return Array.from(programas).filter(programaProgramacionDiaria => {
+        const rect = programaProgramacionDiaria.getBoundingClientRect();
+        return rect.top < window.innerHeight && rect.bottom > 0;
+    });
+}
+
+
 const contenedores = document.querySelectorAll(".mensaje-ondemand-contenido");
 
 contenedores.forEach(contenedor => {
